d279897060850dcaac94de19f7ec65c2e7914764
Augustine Victor <augustinevickky@gmail.com>
2021-06-15 09:52:15 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bmi_calculator/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'landing_page_ui.dart';
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
           appBarTheme: AppBarTheme(
             backgroundColor: Color(0xff0a0d22),
           )),
-      home: HomeScreen(),
+      home: MyHomePage(title: ""),
     );
   }
 }
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
 //User: openvpn
 //Color(0xff04040c),Color(0xff6f7693),Color(0xfffd0225)
 //https://dribbble.com/shots/4585382-Simple-BMI-Calculator/attachments/1036693?mode=media
+//8Ez9b5M
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -38,63 +40,91 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-//8Ez9b5M
 
 class _MyHomePageState extends State<MyHomePage> {
-  int currentPosition = 0;
-  int maxPosition = 5;
-  double get percent => currentPosition / maxPosition;
+  static const platform = const MethodChannel('test.flutter.dev/battery');
+  int batteryLevel = 0;
+
+  int _batteryLevel = 0;
+  Future<void> _getBatterLevel() async {
+    try {
+      final int result = await platform.invokeMethod("getBatteryLevel");
+      batteryLevel = result;
+    } on PlatformException catch (e) {
+      throw Exception(e.message);
+    }
+    setState(() => _batteryLevel = batteryLevel);
+  }
+
+  //int currentPosition = 0;
+  int maxPosition = 100;
+  // double get percent => currentPosition / maxPosition;
+  double get percentage => _batteryLevel / maxPosition;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Determine battery level in android'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            AnimatedDefaultTextStyle(
-                child: Text('${(percent) * 100} %'),
-                style: TextStyle(fontSize: 22),
-                duration: Duration(seconds: 1)),
+            Text(
+              '$batteryLevel%',
+              style: TextStyle(fontSize: 35),
+            ),
             const SizedBox(
               height: 12,
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: AnimatedCounter(
-                percent: percent,
+                percent: percentage,
               ),
-            )
+            ),
+            SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 50,
+                width: 150,
+                color: Colors.white,
+                child: TextButton(
+                    onPressed: () {
+                      _getBatterLevel();
+                    },
+                    child: Text('Get battery Level')),
+              ),
+            ),
           ],
         ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                if (currentPosition < maxPosition) currentPosition++;
-              });
-            },
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          SizedBox(width: 10),
-          FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                if (currentPosition > 0) currentPosition--;
-              });
-            },
-            tooltip: 'decrement',
-            child: Icon(Icons.remove),
-          ),
-        ],
-      ),
+      // floatingActionButton: Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+      //   children: [
+      //     FloatingActionButton(
+      //       onPressed: () {
+      //         setState(() {
+      //           if (currentPosition < maxPosition) currentPosition++;
+      //         });
+      //       },
+      //       tooltip: 'Increment',
+      //       child: Icon(Icons.add),
+      //     ),
+      //     SizedBox(width: 10),
+      //     FloatingActionButton(
+      //       onPressed: () {
+      //         setState(() {
+      //           if (currentPosition > 0) currentPosition--;
+      //         });
+      //       },
+      //       tooltip: 'decrement',
+      //       child: Icon(Icons.remove),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
